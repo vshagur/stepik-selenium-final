b21fffce776b317edd3a47f17d140acd4b9a7993
Valeriy Shagur <vshagur@gmail.com>
2020-05-01 09:39:08 +0700

    move locators to locators.py

--- stepik_selenium_final/pages/main_page.py
+++ stepik_selenium_final/pages/main_page.py
@@ -1,12 +1,12 @@
-from selenium.webdriver.common.by import By
+from stepik_selenium_final.locators import MainPageLocators
 
 from stepik_selenium_final.pages import BasePage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), \
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), \
             "Login link is not presented"
