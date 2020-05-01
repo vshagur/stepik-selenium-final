567b454d7add36b55e3bbcda608705caed81a77e
Valeriy Shagur <vshagur@gmail.com>
2020-05-01 09:29:48 +0700

    add test_guest_should_see_login_link

--- stepik_selenium_final/pages/base_page.py
+++ stepik_selenium_final/pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
@@ -5,3 +8,10 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, by, locator):
+        try:
+            self.browser.find_element(by, locator)
+        except NoSuchElementException:
+            return False
+        return True
--- stepik_selenium_final/pages/main_page.py
+++ stepik_selenium_final/pages/main_page.py
@@ -6,3 +6,7 @@ from stepik_selenium_final.pages import BasePage
 class MainPage(BasePage):
     def go_to_login_page(self):
         self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), \
+            "Login link is not presented"
